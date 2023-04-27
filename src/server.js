8b8050e0346a72b807254a1bc4a485b5941caea5
twitoorel1@gmail.com <twitoorel1@gmail.com>
2023-04-27 19:54:24 +0300

    update path

--- src/server.js
+++ src/server.js
@@ -26,10 +26,12 @@ app.use(cors({
 
 // app.use("/", routes);
 
-app.use("/", express.static(path.resolve(__dirname, "./client/build")));
+app.use("/", express.static(path.resolve(path.dirname("./client/build"))));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(path.dirname("./client/build"), "index.html"));
 });
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Working ON => http://localhost:${port}`));
